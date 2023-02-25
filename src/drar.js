f02972d6e687dd137010a9b98f77af67d7aab6a4
cecil <cecilghimire@gmail.com>
2023-02-25 21:13:22 +0545

    aded edit mode button

--- src/drar.js
+++ src/drar.js
@@ -17,7 +17,7 @@ import "leaflet/dist/leaflet.css";
 import "leaflet-draw/dist/leaflet.draw.css";
 
 import "leaflet-draw";
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 
 import wk from "wellknown";
@@ -123,10 +123,52 @@ export function Drar() {
     featureGroup.clearLayers();
     setPolygonCoords(null);
   }
+  const [analysisResult, setAnalysisResult] = useState("");
+const fetchBufferPolygon = async () => {
+    console.log('fecthcing......')
+    const data = await fetch(
+      `http://127.0.0.1:8000/building/?lat=27.6588&lng=85.3247&buffer_distance=1000`
+    );
+    const affectedBuilding = await data.json();
+    setAnalysisResult(affectedBuilding);
+  };
+useEffect(()=>{
+  if (Editmode&& !analysisResult){  
+    fetchBufferPolygon()}
+     console.log('userffect run')
+  },[Editmode])
 
+  function createPopupContent(feature) {
+    return (
+      "<h3>" +
+      "OSM ID FOR THE BUIDING" +
+      "</h3>" +
+      "<p>" +
+      feature?.properties?.osm_id +
+      "</p>" +
+      "<p> Amenities" +
+      feature?.properties?.amenities +
+      "</p>"
+    );
+  }
+
+  function onEachFeature(feature, layer) {
+    var color =
+      feature.properties.amenities == "place_of_worship" ? "blue" : "red";
+    var fillcolor =
+      feature.properties.amenities == "place_of_worship" ? "blue" : "";
+      
+    layer.setStyle({
+      fillColor: fillcolor,
+      color: color,
+      weight: 1,
+      fillOpacity: 0.7,
+    });
+    layer.bindPopup(createPopupContent(feature));
+  }
   return (
     <> 
-    <button className={Editmode?"bg-green-700 text-white p-1":"p-1 bg-red-700 text-white"} onClick={()=>setEditmode(!Editmode)}>{Editmode?"Edit mode enable":"Edit mode disable"}</button>
+    <button className={Editmode?"bg-green-700 text-white p-1 pr-2":"p-1 bg-red-700 text-white"} onClick={()=>setEditmode(!Editmode)}>{Editmode?"Edit mode enable":"Edit mode disable"}</button>
     <div className="grid grid-cols-4 ">
      
       <div className="col-span-2">
@@ -150,6 +192,9 @@ export function Drar() {
           </div>
           <br />
           <br />
+          {Editmode&&analysisResult && (
+        <GeoJSON data={analysisResult} onEachFeature={onEachFeature} />
+      )}
           <div className="my-container"></div>
         </MapContainer>
       </div>
